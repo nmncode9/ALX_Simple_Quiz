1af041d54c8305d61b225500859ec491f9d228a0
numan <197640394+nmncode9@users.noreply.github.com>
2025-06-15 17:31:58 +0100

    Add core logic to the quiz

--- quiz.js
+++ quiz.js
@@ -1,1 +1,16 @@
+function checkAnswer() {
+    let correctAnswer = "4";
 
+    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    const feedback = document.getElementById("feedback");
+
+    if (userAnswer.value == correctAnswer) {
+        feedback.textContent = "Correct! Well done."
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+    
+}
+
+const button = document.getElementById("submit-answer");
+button.addEventListener("click", checkAnswer);
